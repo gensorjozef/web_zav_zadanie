9193a83b116befdde55ac3ef2950acbe76a0dd95
unknown <tomas.singi.singhofer@gmail.com>
2020-12-27 17:19:07 +0100

    pridany breadcrumbs webkomponent

---
+++ components/breadcrumbs.js
@@ -0,0 +1,76 @@
+/* Breadcrumbs
+	webkomponent
+		- <bread-crumbs></bread-crumbs>
+		- <div id="breadcrumbs"> <a> <a> .. </div>
+
+*/
+
+
+function setBreadcrumbs(title, location){
+	let data = updateBreadcrumbs(title, location);
+	data = deleteBreadcrumb(data);
+	saveBreadcrumbs(data);
+	return createBreadcrumbs(data);
+}
+
+// Update
+// nacita data zo sessionStoragu a prida aktualnu stranku
+function updateBreadcrumbs(title, location){
+	let data = JSON.parse(sessionStorage.getItem('data'));
+	if(data === null){
+		data = new Array(2);
+	}
+
+	if(!data[0]){
+		data[0] = [title];
+		data[1] = [location];
+	}else{
+		data[0][data[0].length] = title;
+		data[1][data[1].length] = location;
+	}
+
+	return data;
+}
+
+// Delete
+// vymaze posledne linky ak je v histori viac ako 5 stranok
+function deleteBreadcrumb(data){
+	if(data[0].length > 5){
+		data[0].splice(0, data[0].length-5);
+		data[1].splice(0, data[1].length-5);
+	}
+
+	return data;
+}
+
+// Save
+// ukladanie do sessionStoragu
+function saveBreadcrumbs(data){
+	sessionStorage.setItem('data',JSON.stringify(data));
+}
+
+// Create
+// Vytvorenie stringu, pouzivany pri vytvarani webkomponentu
+function createBreadcrumbs(data){
+	let string = "<div id='breadcrumbs'>";
+	for (var i = 0; i < data[0].length; i++) {
+		string += "<a href='"+data[1][i]+"'>"+data[0][i]+"</a>>"
+	}
+	string += "</div>";
+	return string;
+}
+
+// Webkomponent
+class Breadcrumbs extends HTMLElement{
+
+	constructor(){
+		super();
+		this.breadcrumbs = setBreadcrumbs(document.title, document.location.href);
+	}
+
+    connectedCallback(){
+        this.innerHTML = this.breadcrumbs;
+    };
+}
+
+customElements.define('bread-crumbs', Breadcrumbs);
